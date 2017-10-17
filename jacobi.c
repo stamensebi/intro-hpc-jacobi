96adb493c451e95d5bfff39b2c290397a375c0af
Sebastian-Stefan Stamen <ss15807@it000444.users.bris.ac.uk>
2017-10-17 14:33:37 +0100

    reversed

--- jacobi.c
+++ jacobi.c
@@ -27,7 +27,7 @@
 static int N;
 static int MAX_ITERATIONS;
 static int SEED;
-static float CONVERGENCE_THRESHOLD;
+static double CONVERGENCE_THRESHOLD;
 
 #define SEPARATOR "------------------------------------\n"
 
@@ -39,14 +39,14 @@ void parse_arguments(int argc, char *argv[]);
 
 // Run the Jacobi solver
 // Returns the number of iterations performed
-int run(float *A, float *b, float *x, float *xtmp)
+int run(double *A, double *b, double *x, double *xtmp)
 {
   int itr;
   int row, col;
-  float dot;
-  float diff;
-  float sqdiff;
-  float *ptrtmp;
+  double dot;
+  double diff;
+  double sqdiff;
+  double *ptrtmp;
 
   // Loop until converged or maximum iterations reached
   itr = 0;
@@ -87,10 +87,10 @@ int main(int argc, char *argv[])
 {
   parse_arguments(argc, argv);
 
-  float *A    = malloc(N*N*sizeof(float));
-  float *b    = malloc(N*sizeof(float));
-  float *x    = malloc(N*sizeof(float));
-  float *xtmp = malloc(N*sizeof(float));
+  double *A    = malloc(N*N*sizeof(double));
+  double *b    = malloc(N*sizeof(double));
+  double *x    = malloc(N*sizeof(double));
+  double *xtmp = malloc(N*sizeof(double));
 
   printf(SEPARATOR);
   printf("Matrix size:            %dx%d\n", N, N);
@@ -104,15 +104,15 @@ int main(int argc, char *argv[])
   srand(SEED);
   for (int row = 0; row < N; row++)
   {
-    float rowsum = 0.0;
+    double rowsum = 0.0;
     for (int col = 0; col < N; col++)
     {
-      float value = rand()/(float)RAND_MAX;
+      double value = rand()/(double)RAND_MAX;
       A[row + col*N] = value;
       rowsum += value;
     }
     A[row + row*N] += rowsum;
-    b[row] = rand()/(float)RAND_MAX;
+    b[row] = rand()/(double)RAND_MAX;
     x[row] = 0.0;
   }
 
@@ -122,10 +122,10 @@ int main(int argc, char *argv[])
   double solve_end = get_timestamp();
 
   // Check error of final solution
-  float err = 0.0;
+  double err = 0.0;
   for (int row = 0; row < N; row++)
   {
-    float tmp = 0.0;
+    double tmp = 0.0;
     for (int col = 0; col < N; col++)
     {
       tmp += A[row + col*N] * x[col];
@@ -167,10 +167,10 @@ int parse_int(const char *str)
   return strlen(next) ? -1 : value;
 }
 
-float parse_float(const char *str)
+double parse_double(const char *str)
 {
   char *next;
-  float value = strtod(str, &next);
+  double value = strtod(str, &next);
   return strlen(next) ? -1 : value;
 }
 
@@ -186,7 +186,7 @@ void parse_arguments(int argc, char *argv[])
   {
     if (!strcmp(argv[i], "--convergence") || !strcmp(argv[i], "-c"))
     {
-      if (++i >= argc || (CONVERGENCE_THRESHOLD = parse_float(argv[i])) < 0)
+      if (++i >= argc || (CONVERGENCE_THRESHOLD = parse_double(argv[i])) < 0)
       {
         printf("Invalid convergence threshold\n");
         exit(1);
